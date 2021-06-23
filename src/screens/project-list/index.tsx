6b331b4c6e7c577a6d9f13695990b07264877108
hhhhhq <Mr.j@tom.com>
2021-06-23 10:49:41 +0800

    feat: search-panel

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,22 +1,21 @@
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import React, { useState } from "react";
+import React from "react";
 import { useDebounce, useDocumentTitle } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "../../utils/project";
 import { useUsers } from "../../utils/user";
+import { useUrlQueryParam } from "../../utils/url";
 
 export const ProjectListScreen = () => {
-  const [param, setParam] = useState({
-    name: "",
-    personId: "",
-  });
+  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
   const debounceParam = useDebounce(param, 2000);
   const { isLoading, error, data: list } = useProjects(debounceParam);
   const { data: users } = useUsers();
 
   useDocumentTitle("项目列表", false);
+  // console.log(useUrlQueryParam(['name']));
 
   return (
     <Container>
